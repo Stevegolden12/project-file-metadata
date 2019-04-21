314ca94f447cfc3f9d29b7d5211ac52f5c8e3e7b
Steve Golden <sgolden2525@outlook.com>
2019-04-21 02:25:30 -0500

    file upload
    Added file upload ability from multer.
    Added message showing file name and size in KB.
    Added easier user file name system using shortid node package.

--- server.js
+++ server.js
@@ -2,6 +2,21 @@
 
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer')
+const shortid = require('shortid')
+
+//change the filename to something more user friendly
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, '');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + "-" + shortid.generate());
+  }
+});
+
+var upload = multer({ storage: storage })
+
 
 // require and use "multer"...
 
@@ -17,6 +32,13 @@ app.get('/', function (req, res) {
 app.get('/hello', function (req, res) {
   res.json({ greetings: "Hello, API" });
 });
+
+//File upload route
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  //req.file.filename = shortid.generate;
+  const kbSize = Math.floor(req.file.size / 1000);
+  res.send("The file is saved as: " + req.file.filename + " and size is " + kbSize + " KB.")
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
